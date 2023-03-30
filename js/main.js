3748c5b104ab2ac09e09fecb5d3fe18186c9e89a
AleksF2022 <aleksfomin52@gmail.com>
2023-03-30 18:10:40 +0300

    heroes and footer completed

--- js/main.js
+++ js/main.js
@@ -5,4 +5,20 @@ $(function(){
       showinfo: 0
     }
   });
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button  class="slick-prev slick-btn"><img src="images/arrow-left.png" alt=""></img></button>',
+    nextArrow: '<button  class="slick-next slick-btn"><img src="images/arrow-right.png" alt=""></img></button>',
+    asNavFor: '.heroes__slider-text'
+  });
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-img',
+    fade: true,
+    arrows: false
+    
+  });
+
 });
